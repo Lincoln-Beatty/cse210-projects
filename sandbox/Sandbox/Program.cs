a5b1b8ed4d1f1b70b2d40c5adaa5909001a28a2c
Lincoln Beatty <lincolngeorgebeatty@gmail.com>
2025-02-25 09:49:45 -0700

    Figuring out input for c#

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,6 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Lincoln's World!");
-        Console.WriteLine("Trying to understand this?");
+        Console.WriteLine("Trying to understand this, do you? ");
+        string answer = Console.ReadLine();
+        Console.WriteLine($"{answer}! That is what you think!?!");
     }
 }
