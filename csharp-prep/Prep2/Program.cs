e64e5e6e7db10858ff882c40116063315a2e4cf8
Lincoln Beatty <lincolngeorgebeatty@gmail.com>
2025-02-27 09:46:20 -0700

    A letter grading program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+class Program{
+    static void Main(string[] args){
+        Console.Write("What is your grade percentage? %");
+        string userinput = Console.ReadLine();
+        int number = int.Parse(userinput);
+        string letter_grade;
+        if (number >= 70){
+            if (number <= 80)
+            {
+                letter_grade = "C";
+            }
+            else if (number <= 90)
+            {
+                letter_grade = "B";
+            }
+            else if (number <= 100)
+            {
+                letter_grade = "A";
+            }
+            else{
+                letter_grade = "invalid";
+            }
+            Console.WriteLine($"You passed with a {letter_grade!}");
+        }
+        else{
+            if (number >= 60)
+            {
+                letter_grade = "C";
+            }
+            else 
+            {
+                letter_grade = "F";
+            }
+            Console.WriteLine($"You failed with a {letter_grade}");
+        }
     }
 }
