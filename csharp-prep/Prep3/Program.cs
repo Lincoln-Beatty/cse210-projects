9ee1443d963de21f6bf2742546cbe8d2db7f49a5
Lincoln Beatty <lincolngeorgebeatty@gmail.com>
2025-03-01 11:39:21 -0700

    Guessing the number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Concurrent;
+using System.Xml.Serialization;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program{
+    static void Main(string[] args){
+        Random random = new Random();
+        int random_number = random.Next(1, 100);
+        // Console.Write("What is the magic number? ");
+        int magic_number = random_number;
+        bool correct = false;
+        do{
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess > magic_number){
+                Console.WriteLine("Lower");
+            }else if (guess < magic_number){
+                Console.WriteLine("Higher");
+            }else{
+                correct = true;
+                Console.WriteLine("You guessed it!");
+            }
+        } while (correct != true);
     }
 }
