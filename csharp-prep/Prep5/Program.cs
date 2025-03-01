c45c17357636795671c460687b68a2e763fb8b01
Lincoln Beatty <lincolngeorgebeatty@gmail.com>
2025-03-01 12:14:53 -0700

    using basic functions and values

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+class Program{
+    static void Main(string[] args){
+        welcome();
+        string name = Your_name();
+        int number = Fav_number();
+        int square = Squared(number);
+        Display_results(name, square);
+    }
+    static void welcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string Your_name(){
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int Fav_number(){
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int Squared(int number){
+        int square = number * number;
+        return square;
+    }
+    static void Display_results(string name, int square){
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
