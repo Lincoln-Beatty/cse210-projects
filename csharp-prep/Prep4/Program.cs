3acd0ff93bc784d07cc5477a724b6d31fb7dbaec
Lincoln Beatty <lincolngeorgebeatty@gmail.com>
2025-03-01 11:59:58 -0700

    adding a list of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int number;
+        do{
+            
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0){
+                numbers.Add(number);
+            }
+        } while (number != 0);
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+        int average = sum / numbers.Count();
+        Console.WriteLine($"The average is: {average}");
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is: {largest}");
+  //The sum is: 131
+  //The average is: 18.714285714285715
+  //The largest number is: 48");
     }
 }
